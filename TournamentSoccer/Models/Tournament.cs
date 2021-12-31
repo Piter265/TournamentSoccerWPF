825eea0f731633655b1eb57e55c4a07ee96135f1
Piter265 <p.anuszkiewicz527@gmail.com>
2021-12-31 10:30:15 +0100

    Create initial models

--- TournamentSoccer/Models/Tournament.cs
+++ TournamentSoccer/Models/Tournament.cs
@@ -6,9 +6,34 @@ using System.Threading.Tasks;
 
 namespace TournamentSoccer.Models
 {
-    class Tournament
+    sealed class Tournament
     {
-        public string Name { get; set; }
-        public bool Rematches { get; set; }
+        public readonly string Name;
+        public readonly bool Rematches;
+        private static readonly object _lock = new object();
+
+        private static Tournament _instance;
+
+        private Tournament(string name, bool rematches)
+        {
+            Name = name;
+            Rematches = rematches;
+        }
+
+        public static Tournament GetInstance(string name, bool rematches)
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Tournament(name, rematches);
+                    }
+                }
+            }
+
+            return _instance;
+        }
     }
 }
