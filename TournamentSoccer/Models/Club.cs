8e261ff0061c0db69cb9e325f542ce26c691bf3a
Piter265 <p.anuszkiewicz527@gmail.com>
2022-01-03 12:14:01 +0100

    Add views

---
+++ TournamentSoccer/Models/Club.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentSoccer.Models
+{
+    class Club
+    {
+        public string Name;
+        public int Wins;
+        public int Losses;
+        public int Draws;
+        private List<Player> Players;
+
+        public Club(string name, int wins, int losses, int draws)
+        {
+            Name = name;
+            Wins = wins;
+            Losses = losses;
+            Draws = draws;
+            Players = new List<Player>();
+        }
+
+        public void AddPlayer(Player player)
+        {
+            Players.Add(player);
+        }
+    }
+}
--- TournamentSoccer/Models/Match.cs
+++ TournamentSoccer/Models/Match.cs
@@ -8,5 +8,28 @@ namespace TournamentSoccer.Models
 {
     class Match
     {
+        public int Goals { get; set; }
+        public int Assists { get; set; }
+        public int Shots { get; set; }
+        public int ShotsOnTarget { get; set; }
+        public int YellowCards { get; set; }
+        public int RedCards { get; set; }
+
+        private List<Player> Squad1;
+        private List<Player> Squad2;
+
+
+        public Match(int goals, int assists, int shots, int shotsOnTarget, int yellowCards, int redCards)
+        {
+            Goals = goals;
+            Assists = assists;
+            Shots = shots;
+            ShotsOnTarget = shotsOnTarget;
+            YellowCards = yellowCards;
+            RedCards = redCards;
+            Squad1 = new List<Player>();
+            Squad2 = new List<Player>();
+
+        }
     }
 }
