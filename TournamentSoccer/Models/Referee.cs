4d096476e6842dfccf7e8b58a3614761540d8cd3
Piter265 <p.anuszkiewicz527@gmail.com>
2022-01-17 15:49:26 +0100

    Facade

--- TournamentSoccer/Models/Referee.cs
+++ TournamentSoccer/Models/Referee.cs
@@ -8,9 +8,14 @@ namespace TournamentSoccer.Models
 {
     public class Referee : Person
     {
-        public Referee(string name, string lastName, string function, int age):base(name, lastName, function, age)
+        public Referee()
         {
 
+        }
+
+        public static List<Person> LoadRefeeres()
+        {
+            return new List<Person>();
         }
     }
 }
---
+++ TournamentSoccer/Models/UniqueTeamBuilder.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentSoccer.Models
+{
+    public class UniqueTeamBuilder : ITeamBuilder
+    {
+        private LinkedList<Person> _people = new LinkedList<Person>();
+
+        public UniqueTeamBuilder()
+        {
+            this.Reset();
+        }
+
+        public void Reset()
+        {
+            this._people = new LinkedList<Person>();
+        }
+
+        public void AddCoach()
+        {
+            _people.AddFirst(CreatePlayer());
+        }
+
+        public void AddPlayer()
+        {
+            _people.AddFirst(CreatePlayer());
+        }
+
+        public Coach CreateCoach()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Person CreatePlayer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public bool SetDisability(bool isDisable)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetGroupAge(string groupAge)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
