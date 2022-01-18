f2a04f4be080d8011cb53b603872d7f9ecaf1c13
Piter265 <p.anuszkiewicz527@gmail.com>
2022-01-18 17:32:30 +0100

    new

--- TournamentSoccer/Models/Tournament.cs
+++ TournamentSoccer/Models/Tournament.cs
@@ -10,9 +10,9 @@ namespace TournamentSoccer.Models
     {
         public string Name { get; set; }
         public bool Rematches { get; set; }
-        public static List<Match> Matches { get; set; } = new List<Match>();
-        public static List<Team> Teams { get; set; } = new List<Team>();
-        public static List<Referee> Referees { get; set; } = new List<Referee>();
+        public List<Match> Matches { get; set; } = new List<Match>();
+        public List<Team> Teams { get; set; } = new List<Team>();
+        public List<Referee> Referees { get; set; } = new List<Referee>();
         private static readonly object _lock = new object();
 
         private static Tournament _instance;
@@ -39,24 +39,24 @@ namespace TournamentSoccer.Models
         }
 
 
-        public static List<Match> DrawMatches(List<Team> clubs)
+        public List<Match> DrawMatches(List<Team> clubs)
         {
             List<Match> matches = new List<Match>();
 
             return matches;
         }
 
-        public static void AddTeam(Team team)
+        public void AddTeam(Team team)
         {
             Teams.Add(team);
         }
 
-        public static void DeleteTeam(Team team)
+        public void DeleteTeam(Team team)
         {
             Teams.Remove(team);
         }
 
-        public static void LoadRefeeres()
+        public void LoadRefeeres()
         {
             
         }
